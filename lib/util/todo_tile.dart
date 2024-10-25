ac122ca9e9bcf4caec0868bc0db507ce5c0fd009
Christian Teguim <cteguimtu620@gmail.com>
2024-10-25 12:26:33 -0400

    fixed app layout added checkbox components and mandatory varaibles that need to be initialized

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatelessWidget {
-  const TodoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.yellow),
-      child: Text("Make my bed"),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.amber[300],
+            ),
+
+            //task name
+            Text(taskName),
+          ],
+        ),
+      ),
     );
   }
 }
